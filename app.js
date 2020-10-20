6b5e833ade6d9ba44e2475dd82d32602c0af42c4
Smartniggs <smartniggs@gmail.com>
2020-10-20 16:37:43 +0100

    updated files, removed comments

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const port = process.env.PORT || 5000;
 const dogRoute = require("./routes/dog.route");
 
 const app = express();
-// const router = app.router();
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-
+// dog route
 app.use('/dog', dogRoute);
 
 app.listen(port, () => console.log(`Express Server started on ${port}`));
